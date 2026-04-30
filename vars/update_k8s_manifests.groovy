aa35eb7dbdee25269cdb8c1126203502b1c8c1a3
satya2330 <93843037+satya2330@users.noreply.github.com>
2026-04-30 08:47:46 -0500

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -13,7 +13,7 @@ def call(Map config = [:]) {
     echo "Updating Kubernetes manifests with image tag: ${imageTag}"
     
     withCredentials([usernamePassword(
-        credentialsId: 'GIT-jenkins',
+        credentialsId: gitCredentials,
         usernameVariable: 'GIT_USERNAME',
         passwordVariable: 'GIT_PASSWORD'
     )]) {
